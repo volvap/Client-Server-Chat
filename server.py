232d9df1b82475b3f7cfe231f82c8765099a9b86
Vladislav Pavlov <48180431+volvap@users.noreply.github.com>
2020-04-06 20:05:12 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -8,24 +8,28 @@ PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER,PORT)
 FORMAT = "utf-8"
-DISSCONNET_MESSAGE = "!DISCONNET"
+DISCONNET_MESSAGE = "!DISCONNET"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected")
+    print(f"[NEW CONNECTION] {addr} connected.")
 
     connected = True
     while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT) 
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg == DISSCONNET_MESSAGE:
-            connected = False
-        print(f"[{addr}] {msg}")
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNET_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
 
     conn.close()
+
 
 def start():
     server.listen()

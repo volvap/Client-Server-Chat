df69b222562dab14455e0ded6216185ff6931ca5
Vladislav Pavlov <48180431+volvap@users.noreply.github.com>
2020-04-06 19:06:11 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -3,20 +3,37 @@ import threading
 import time
 
 
+HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER,PORT)
+FORMAT = "utf-8"
+DISSCONNET_MESSAGE = "!DISCONNET"
 
 server = socket.socket(socket.AF_INIT, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 def handle_client(conn, addr):
-    pass
+    print(f"[NEW CONNECTION] {addr} connected")
 
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == DISSCONNET_MESSAGE:
+            connected = False
+        print(f"[{addr}] {msg}")
+
+    conn.close()
 
 def start():
-    pass
-
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 print("[STARTING] server is starting....")
 start()
